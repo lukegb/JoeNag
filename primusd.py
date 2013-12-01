fa16cdb8a875beadc812d14fc2cf4c1dde83d007
Luke GB <git@lukegb.com>
2013-12-01 21:01:15 +0000

    Add some magical automatic updating stuff

--- primusd.py
+++ primusd.py
@@ -2,15 +2,29 @@
 
 import traceback
 import time
+import subprocess
+import os.path
 
-CHECK_GIT_FOR_UPDATES_EVERY = 30
+CHECK_GIT_FOR_UPDATES_EVERY_MINUTES = 1
 
+update_git_timer = 0
 
 print "Starting up..."
 while True:
     try:
+        now = time.time()
+        if (now - (CHECK_GIT_FOR_UPDATES_EVERY_MINUTES*60)) > update_git_timer:
+            print "Checking for updates..."
+            if os.path.exists('../updater.sh'):
+                subprocess.check_call(["../updater.sh"])
+            else:
+                print "Can't check for updates, no updater.sh :("
         print "!"
         time.sleep(20)
+    except subprocess.CalledProcessError:
+        print "TERMINATING DUE TO CalledProcessError!"
+        import sys
+        sys.exit(0)
     except Exception, ex:
         traceback.print_exc()
         continue
